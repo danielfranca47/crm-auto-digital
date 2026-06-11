2181e059bf951f22254ba8ae87af414fdd47b8ee
danielfranca47 <danielhsfranca@gmail.com>
2026-06-11 18:38:44 +0100

    chore(auto): alterações pendentes na branch etapa-9-planos-limites

--- backend-core/app/main.py
+++ backend-core/app/main.py
@@ -13,11 +13,13 @@ _scheduler = None
 origins = [
     "http://localhost:5173",
     "http://localhost:5174",
+    "http://localhost:5176",
     "http://localhost:3000",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "http://127.0.0.1:5176",
 ]
 
 app.add_middleware(
